73554d884e76515e0e9e4107a6101f0f957750f5
Brahm <91026999+brahm23@users.noreply.github.com>
2026-07-16 18:12:12 +0530

    Add files via upload

---
+++ Codes/15 July/SeleniumWebDriverLocators/src/test/java/com/SeleniumWebDriver/LocatorsDemo/LocatorClassNameTest.java
@@ -0,0 +1,33 @@
+package com.SeleniumWebDriver.LocatorsDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LocatorClassNameTest {
+	WebDriver driver;
+	
+	@Test
+	
+	public void classTest() throws InterruptedException {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.hollandandbarrett.com/shop/vitamins-supplements/vitamins/vitamin-drinks/");
+		try {
+			driver.findElement(By.id("onetrust-accept-btn-handler")).click();	
+		} catch (Exception e) {
+			System.out.println("Cookie popup did not appear");
+		}
+		
+		Thread.sleep(1000);
+		driver.findElement(By.className("ProductCard-module_title__cGT4y")).click();
+		Thread.sleep(1000);
+		driver.navigate().back();
+		Thread.sleep(1000);
+		driver.navigate().forward();
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+}
---
+++ Codes/15 July/WebDriverNavigationCommands/src/test/java/com/navigationcommands/Demo/A_NavigationCommands.java
@@ -0,0 +1,74 @@
+package com.navigationcommands.Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class A_NavigationCommands {
+	WebDriver driver;
+	
+//	@Test (priority=0)
+//	public void NavigateDemo() {
+//		driver = new ChromeDriver();
+//		driver.navigate().to("https://practicetestautomation.com/practice-test-login/");
+//		String URL = driver.getCurrentUrl();
+//		System.out.println(URL);
+//		driver.navigate().back();
+//	}
+//
+//	@Test (priority=1)
+//	public void NavigateBackDemo(){
+//		driver = new ChromeDriver();
+//
+//        driver.get("https://www.google.co.in");
+//        String URL = driver.getCurrentUrl();
+//        System.out.println(URL);
+//
+//        String Title = driver.getTitle();
+//        System.out.println(Title);
+//
+//        driver.navigate().to("https://login.yahoo.com/");
+//        URL = driver.getCurrentUrl();
+//        System.out.println(URL);
+//
+//        driver.navigate().back();
+//        URL = driver.getCurrentUrl();
+//        System.out.println(URL);
+//	}
+//	
+//	@Test (priority=2)
+//	
+//	public void NavigateForwardDemo() {
+//		driver.get("https://www.google.co.in");
+//		String URL = driver.getCurrentUrl();
+//		System.out.println(URL);
+//		
+//		driver.navigate().to("https://login.yahoo.com/");
+//		URL = driver.getCurrentUrl();
+//        System.out.println(URL);
+//        
+//        driver.navigate().forward();
+//        URL = driver.getCurrentUrl();
+//        System.out.println(URL);
+//	}
+	
+//	@Test (priority=3)
+//	
+//	public void NavigateRefreshDemo() {
+//		driver = new ChromeDriver();
+//		driver.get("https://www.google.co.in");
+//		String URL = driver.getCurrentUrl();
+//		System.out.println(URL);
+//		driver.navigate().to("https://www.google.co.in");
+//		driver.navigate().refresh();
+//	}
+	
+	
+	
+	
+
+
+}
